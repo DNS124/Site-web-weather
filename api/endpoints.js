ccaadbe393905c805e7e88404c2d07de78a588a9
DNS124 <135117967+DNS124@users.noreply.github.com>
2025-02-23 15:11:56 +0200

    Add files via upload

---
+++ api/endpoints.js
@@ -0,0 +1,12 @@
+
+const API_KEY = "d887e6790048e1cc98160cc9acc676e8";
+
+
+function getCurrentWeatherEndpoint(city) {
+
+  return `https://api.openweathermap.org/data/2.5/weather?q=${city}&lang=ro&units=metric&appid=${API_KEY}`;
+}
+
+function getForecastEndpoint(city) {
+  return `https://api.openweathermap.org/data/2.5/forecast?q=${city}&lang=ro&units=metric&appid=${API_KEY}`;
+}
